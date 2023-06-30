da9939780e73af2dc0d143b238b8c1b32cfe7305
OlwethuB <olwethu.a.bhiya@gmail.com>
2023-06-30 16:28:49 +0200

    initial

---
+++ src/components/IntroC.vue
@@ -0,0 +1,28 @@
+<template>
+
+    <div class="intro">
+        <h1>Hi I am Olwethu Bhiya</h1>
+        <h1>Aspiring full stack developer</h1>
+        <br>
+        <h3> I am good at coding and stuff</h3>
+    </div>
+
+</template>
+    
+    <script>
+    import { } from ''
+    
+    export default {
+    
+    }
+    </script>
+    
+    <style>
+    /* Intro section */
+    .intro {
+      background-image: url(https://i.postimg.cc/CLmdXPgw/Bkg-lndscp-1.png);
+      height: auto;
+      text-align: center;
+      width: 100%;
+    }
+    </style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+    <Intro/>
+
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+  <NavSec/>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import Intro from '@/components/IntroC.vue';
+import NavSec from './components/NavSec.vue';
+
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld,Intro,NavSec
+  }
+}
+</script>
