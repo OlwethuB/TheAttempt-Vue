0ae23a10ee8aea3f1eea7b9655e03ad1a5c8f8ab
OlwethuB <130047153+OlwethuB@users.noreply.github.com>
2023-07-07 23:04:28 +0200

    Update

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,65 @@ import HomeView from '../views/HomeView.vue'
 import about from "../views/AboutView.vue"
 import resume from "../views/ResumeView.vue"
 import Skills from "../components/Skills.vue"
+import projects from "../views/ProjectsView.vue"
+import testimonials from "../views/TestimonialsView.vue"
+import contact from "../views/ContactView.vue"
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: about,
+    meta: { section: "about" },
+  },
+  {
+    path: '/skills',
+    name: 'skills',
+    component: Skills,
+    meta: {section: "skills"}
+  },
+  {
+    path: '/resume',
+    name: 'resume',
+    component: resume,
+    meta: {section: "resume"}
+  },
+  {
+    path: '/project',
+    name: 'project',
+    component: projects,
+    meta: {section: "project"}
+  },
+  {
+    path: '/testi',
+    name: 'testi',
+    component: testimonials,
+    meta: {section: "testimonials"}
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: contact,
+    meta: {section: "contact"}
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import about from "../views/AboutView.vue"
+import resume from "../views/ResumeView.vue"
+import Skills from "../components/Skills.vue"
 import Projects from "../views/ProjectsView.vue"
 import Testimonials from "../views/TestimonialsView.vue"
 import Contact from "../views/ContactView.vue"
